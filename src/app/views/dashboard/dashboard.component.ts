f459f9cc0916e36b6184f0de7eea65b8404f535f
Anand R <anand.r@esoft.com>
2018-02-15 16:10:49 +0530

    firebase json

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,9 +9,12 @@ import { AuthService } from '../../services/auth.service';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService,private router: Router) { }
 
   ngOnInit() {
   }
-
+questionOnClick () {
+  debugger
+  this.router.navigate(['/question']);
+}
 }
